b5651fe94e6d48a3107ee7a687d721d7892eb99a
Kumar <prem7696288994l@gmail.com>
2023-09-16 12:10:39 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+const App = () => {
+  return (
+    <div>
+    <h2 className='text-red-500'>Hello</h2>
+    </div>
+  )
+}
+
+export default App;
